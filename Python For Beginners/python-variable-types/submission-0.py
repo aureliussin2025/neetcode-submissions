ef68e3b94f9bb3ac657d840a58a9252feecfbb56
aureliussin2025 <aureliussin.2025@smu.edu.sg>
2026-05-08 19:22:10 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,15 @@
+integer_type = 10
+float_type = 1.2
+bool_type = False
+string_type = "fake"
+list_type =[0]
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
